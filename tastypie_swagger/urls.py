6fb37ae750b7ef82d153dce8047cb53b170566a3
gjcourt <gjcourt@gmail.com>
2016-05-20 13:44:23 -0700

    Fix urls.py file

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,13 +1,19 @@
 try:
-	from django.conf.urls import patterns, include, url
+	from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import patterns, include, url
+	from django.conf.urls.defaults import url
 
-from .views import SwaggerView, ResourcesView, SchemaView
 
-urlpatterns = patterns('',
+from tastypie_swagger.views import (
+    ResourcesView,
+    SchemaView,
+    SwaggerView,
+)
+
+
+urlpatterns = [
     url(r'^$', SwaggerView.as_view(), name='index'),
     url(r'^resources/$', ResourcesView.as_view(), name='resources'),
     url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
     url(r'^schema/$', SchemaView.as_view(), name='schema'),
-)
+]
