b1e192ae0f96f7183c8e93da85041e158d1f7064
gjcourt <gjcourt@gmail.com>
2016-05-31 19:17:03 -0700

    Revert "Test out commenting out api errors"
    This reverts commit 8d1d4d94f0df304961d8b3b08305acd89ca428b1.

--- tastypie_swagger/static/tastypie_swagger/js/lib/swagger.js
+++ tastypie_swagger/static/tastypie_swagger/js/lib/swagger.js
@@ -361,9 +361,8 @@ var SwaggerResource = function(resourceObj, api) {
           return _this.addApiDeclaration(responseObj);
         },
         error: function(response) {
-          console.log('Error', response);
-          // return _this.api.fail("Unable to read api '" +
-          //   _this.name + "' from path " + _this.url + " (server returned " + response.statusText + ")");
+          return _this.api.fail("Unable to read api '" +
+            _this.name + "' from path " + _this.url + " (server returned " + response.statusText + ")");
         }
       }
     };
